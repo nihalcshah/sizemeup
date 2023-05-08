a5db8b0aead257b201fb84cc0c64cf4b16d2fe5f
nihalcshah <nihalcshah@gmail.com>
2023-05-08 13:28:31 -0400

    s

--- sizemeup/urls.py
+++ sizemeup/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sizing.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 from django.conf import settings
 from django.conf.urls.static import static
